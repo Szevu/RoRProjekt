17eda5122dd5a631121baa4200409db01607eef4
A <a>
2025-01-28 02:32:42 +0100

    a

--- db/migrate/20250127184055_create_join_table_room_category.rb
+++ db/migrate/20250127184055_create_join_table_room_category.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableRoomCategory < ActiveRecord::Migration[8.0]
   def change
     create_join_table :rooms, :categories do |t|
-      # t.index [:room_id, :category_id]
-      # t.index [:category_id, :room_id]
+       t.index [:room_id, :category_id]
+       t.index [:category_id, :room_id]
     end
   end
 end
