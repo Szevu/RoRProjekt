3fe51ceaee6e738202175b741b584d8f7e33bfdc
A <a>
2025-01-28 02:29:27 +0100

    z

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
   has_and_belongs_to_many :categories
   has_many :reservations
-  validates :categories, presence: { message: "Musisz wybrać przynajmniej jedną kategorię" }
+  validates :categories, presence: { message: "Musisz wybrać przynajmniej jedną kategorię lub kategoria musi istnieć"  }
 end
